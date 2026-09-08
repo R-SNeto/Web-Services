5bcab336027962c6069502700710c14ef41ebd00
Neto <rneto879@gmail.com>
2026-09-08 17:52:30 -0300

    Add H2 database

--- src/main/java/com/github/r_sneto/WebServices/entities/User.java
+++ src/main/java/com/github/r_sneto/WebServices/entities/User.java
@@ -1,9 +1,19 @@
 package com.github.r_sneto.WebServices.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -21,7 +31,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
