046be79ec6f51eadf441d71b53cf1c5ae7d706cd
Neto <rneto879@gmail.com>
2026-09-10 15:28:32 -0300

    Added Service Layer and findAll, findById methods

--- src/main/java/com/github/r_sneto/WebServices/resources/UserResource.java
+++ src/main/java/com/github/r_sneto/WebServices/resources/UserResource.java
@@ -1,22 +1,34 @@
 package com.github.r_sneto.WebServices.resources;
 
 import com.github.r_sneto.WebServices.entities.User;
+import com.github.r_sneto.WebServices.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users/")
 public class UserResource {
     //RestController permite que o Spring identifique uma classe como sendo um Controller de padrão Rest
     //RequestMapping nomeia o caminho das requisições em uma classe, atribuindo um valor para ele
     //ResponseEntity é um tipo específico que serve para retornar respostas de requisições web
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        var u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
