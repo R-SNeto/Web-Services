e2d7c531c74d7fc38c3f07b2e6fdc636e14091c7
Neto <rneto879@gmail.com>
2026-09-18 21:52:22 -0300

    Added exceptions handling to the update method in UserService class

--- src/main/java/com/github/r_sneto/WebServices/services/UserService.java
+++ src/main/java/com/github/r_sneto/WebServices/services/UserService.java
@@ -4,6 +4,7 @@ import com.github.r_sneto.WebServices.entities.User;
 import com.github.r_sneto.WebServices.repositories.UserRepository;
 import com.github.r_sneto.WebServices.services.exceptions.DataBaseException;
 import com.github.r_sneto.WebServices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,14 @@ public class UserService {
     }
 
     public User update (Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData (User entity, User obj) {
